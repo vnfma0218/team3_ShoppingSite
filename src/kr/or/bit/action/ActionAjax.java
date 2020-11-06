ec0816fbf06de4d5287b80eed9f38fc86242701a
Ryujeuk <jeuk817@gmail.com>
2020-11-06 22:20:52 +0900

    feat: create ActionAjax
    ActionAjax와 ActionDataAjax를 각각 만들려다가 ActionAjax와 Action이
    겹치는걸 보고 ActionDataAjax를 ActionAjax의 이름으로 만들고 기존
    ActionAjax는 만들지 않았다.

---
+++ src/kr/or/bit/action/ActionAjax.java
@@ -0,0 +1,22 @@
+package kr.or.bit.action;
+
+public class ActionAjax {
+	private String data = null;
+	private String contentType = null;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+}
