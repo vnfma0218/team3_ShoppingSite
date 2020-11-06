6522536e4c26fb289d5e7b7051f6221892040084
Ryujeuk <jeuk817@gmail.com>
2020-11-06 22:17:34 +0900

    feat: create Action.interface, ActionForward

---
+++ src/kr/or/bit/action/ActionForward.java
@@ -0,0 +1,20 @@
+package kr.or.bit.action;
+
+public class ActionForward {
+	private boolean isRedirect = false;
+	private String path=null;
+	private String prePath = "/WEB-INF/views/";
+	
+	public boolean isRedirect() {
+		return isRedirect;
+	}
+	public void setRedirect(boolean isRedirect) {
+		this.isRedirect = isRedirect;
+	}
+	public String getPath() {
+		return prePath + path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
