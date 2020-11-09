3fd8bdc62bf294ab7f5d3836440274068bbcacbd
LeeJongHyeon <asd951415@naver.com>
2020-11-09 11:38:45 +0900

    MODEL : product DAO, DTO Service : ProductAdd 추가

--- src/kr/or/bit/Action/Action.java
+++ src/kr/or/bit/Action/Action.java
@@ -6,5 +6,5 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException;
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException, Exception;
 }
