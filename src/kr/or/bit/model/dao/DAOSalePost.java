0f3bf37fb884cc95890a377a829deaffaf663912
jack <jeuk817@gmail.com>
2020-11-12 16:45:09 +0900

    feat: write sale post complete

--- src/kr/or/bit/model/dao/DAOSalePost.java
+++ src/kr/or/bit/model/dao/DAOSalePost.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import kr.or.bit.model.DBManager;
@@ -18,6 +19,30 @@ public class DAOSalePost {
 	private static final String SQL_SELECT_IMAGES_BY_SALE_NUM = "SELECT IMAGE_ADR FROM IMAGES WHERE SALE_NUM = ?";
 	private static final String SQL_SELECT_SALEPOST_BY_SALE_NUM = "SELECT * FROM SALE_POST WHERE SALE_NUM = ?";
 	private static final String SQL_SELECT_SALEPOSTS_BY_SEL_NUM = "SELECT * FROM SALE_POST WHERE SEL_NUM = ?";
+	private static final String ryu_SQL_INSERT_SALE_POST = "INSERT INTO SALE_POST(SEL_NUM, CATEGORY_NUM, SALE_TITLE, SALE_CONTENT) "
+															+ "VALUES(?, ?, ?, ?)";
+	private static final String ryu_SQL_SELECT_LAST_ROW_FROM_SALE_POST_BY_SEL_NUM = 
+			"SELECT * FROM (SELECT * FROM SALE_POST WHERE SEL_NUM = ? ORDER BY SALE_NUM DESC) WHERE ROWNUM = 1";
+	
+	private static String ryu_getInsertAllPdSalePostQuery(int length) {
+		String insertQuery = "INSERT INTO PD_SALE_POST(SALE_NUM, P_NUM) ";
+		String selectFronDual = "SELECT ?, ? FROM DUAL";
+		for(int i = 0; i <length - 1; i++) {
+			insertQuery += selectFronDual + " UNION ALL ";
+		}
+		
+		return insertQuery + selectFronDual;
+	}
+	
+	private static String ryu_getInsertAllImagesQuery(int length) {
+		String insertQuery = "INSERT INTO IMAGES(SALE_NUM, IMAGE_ADR, IMAGE_MAIN_FLAG) ";
+		String selectFronDual = "SELECT ?, ?, ? FROM DUAL";
+		for(int i = 0; i <length - 1; i++) {
+			insertQuery += selectFronDual + " UNION ALL ";
+		}
+		
+		return insertQuery + selectFronDual;
+	}
 	
 	public static List<DTOSalePost> getSalePostListByCategory(int categoryNum){
 		List<DTOSalePost> salePostList = new ArrayList<DTOSalePost>();
@@ -87,6 +112,71 @@ public class DAOSalePost {
 		}
 		
 		return salePostList;
+	}
+	
+	public static int ryu_insertSalePostAndAll(DTOSalePost salePost) {
+		int resultRow = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = instance.getConnection();
+			pstmt = conn.prepareStatement(ryu_SQL_INSERT_SALE_POST);
+			pstmt.setInt(1, salePost.getSelNum());
+			pstmt.setInt(2, salePost.getCategoryNum());
+			pstmt.setString(3, salePost.getSaleTitle());
+			pstmt.setString(4, salePost.getSaleContent());
+			
+			resultRow = pstmt.executeUpdate();
+//			instance.freeConnection(pstmt);
+			
+			pstmt = conn.prepareStatement(ryu_SQL_SELECT_LAST_ROW_FROM_SALE_POST_BY_SEL_NUM);
+			pstmt.setInt(1, salePost.getSelNum());
+			rs = pstmt.executeQuery();
+			int saleNum = rs.getInt("SALE_NUM");
+			
+			List<Integer> pNums = salePost.getpNums();
+			String insertAllPdSalePostQuery = ryu_getInsertAllPdSalePostQuery(pNums.size());
+			pstmt = conn.prepareStatement(insertAllPdSalePostQuery);
+			ryu_setInsertAllPdSalePostQuery(pstmt, saleNum, pNums);
+			
+			resultRow = pstmt.executeUpdate();
+			
+			List<String> imageAddrs = salePost.getImageAddrs();
+			String insertAllImagesQuery = ryu_getInsertAllImagesQuery(imageAddrs.size());
+			pstmt = conn.prepareStatement(insertAllImagesQuery);
+			ryu_setInsertAllImagesQuery(pstmt, saleNum, imageAddrs);
+			
+			resultRow = pstmt.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			instance.freeConnection(conn, pstmt, rs);
+		}
+		
+		return resultRow;
+	}
+	
+	private static void ryu_setInsertAllImagesQuery(PreparedStatement pstmt, int saleNum, List<String> imageAddrs) throws SQLException {
+		Iterator<String> it = imageAddrs.iterator();
+		int i = 1;
+		while(it.hasNext()){
+			String imageAddr = it.next();
+			pstmt.setInt(i++, saleNum);
+			pstmt.setString(i++, imageAddr);
+			pstmt.setString(i++, i == 4 ? "Y" : "N");			
+		}
+	}
+	
+	private static void ryu_setInsertAllPdSalePostQuery(PreparedStatement pstmt, int saleNum, List<Integer> pNums) throws SQLException {
+		Iterator<Integer> it = pNums.iterator();
+		int i = 1;
+		while(it.hasNext()){
+			int pNum = it.next();
+			pstmt.setInt(i++, saleNum);
+			pstmt.setInt(i++, pNum);
+		}
 	}
 	
 	private static DTOSalePost setDTOSalePost(ResultSet rs) throws SQLException {
--- src/kr/or/bit/service_ajax/WriteSaleAjaxService.java
+++ src/kr/or/bit/service_ajax/WriteSaleAjaxService.java
@@ -1,19 +1,21 @@
 package kr.or.bit.service_ajax;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import kr.or.bit.action.ActionAjax;
 import kr.or.bit.action.ActionAjaxData;
+import kr.or.bit.model.dao.DAOSalePost;
+import kr.or.bit.model.dto.DTOSalePost;
 
 public class WriteSaleAjaxService implements ActionAjax {
 
@@ -27,39 +29,43 @@ public class WriteSaleAjaxService implements ActionAjax {
 //			pNums:[num]
 //			}
 		
-//		INSERT INTO table (column1, column2)
-//		SELECT value1, value2 FROM DUAL UNION ALL
-//		SELECT value1, value2 FROM DUAL UNION ALL
-//		...etc...
-//		SELECT value1, value2 FROM DUAL;
-		
 		ServletContext context = request.getServletContext();
 		String path = context.getRealPath("/upload");
 		System.out.println(path);
 		int size = 1024*1024*10;
 		try {
 			MultipartRequest multiReq = new MultipartRequest(request, path, size, "UTF-8", new DefaultFileRenamePolicy());
+			int selNum = Integer.parseInt((String)request.getSession().getAttribute("sellerNum"));
 			int categoryNum = Integer.parseInt(multiReq.getParameter("categoryNum"));
-			String title = multiReq.getParameter("title");
-			String content = multiReq.getParameter("content");
+			String saleTitle = multiReq.getParameter("title");
+			String saleContent = multiReq.getParameter("content");
+			
+			List<Integer> pNums = new ArrayList<Integer>();
 			String[] pNumsStr = multiReq.getParameter("pNums").split(",");
-			int[] pNums = new int[pNumsStr.length];
 			for(int i = 0; i < pNumsStr.length; i++) {
-				pNums[i] = Integer.parseInt(pNumsStr[i]);
+				pNums.add(Integer.parseInt(pNumsStr[i]));
 			}
 			
+			List<String> imageAddrs = new ArrayList<String>();
 			Enumeration<String> fileNames = multiReq.getFileNames();
 			while(fileNames.hasMoreElements()) {
-				String dd = fileNames.nextElement();
-				System.out.println(dd);
-				System.out.println(multiReq.getOriginalFileName(dd));
+				String file = fileNames.nextElement();
+				String fileSysName = multiReq.getFilesystemName(file);
+				imageAddrs.add(path + "/" + fileSysName);
 			}
+			
+			DTOSalePost salePost = new DTOSalePost(selNum, categoryNum, saleTitle, saleContent, pNums, imageAddrs);
+			int resultRow = DAOSalePost.ryu_insertSalePostAndAll(salePost);
+			if(resultRow == 1) {
+				ajaxData.setData("success");				
+			} else {
+				ajaxData.setData("fail");				
+			}
+			
 		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-//		catch (ServletException e) {
-//			e.printStackTrace();
-//		}
+			System.out.println(e.getMessage());
+			ajaxData.setData("fail");
+		}
 		
 		
 		return ajaxData;
