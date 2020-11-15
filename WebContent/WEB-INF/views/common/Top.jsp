b53ce1b72132026a8b58b15c2d2a0f018d419fb2
Ryujeuk <jeuk817@gmail.com>
2020-11-15 16:35:38 +0900

    feat: Top 네비게이션 url 추가, categoryPageService sampleData 추가

--- WebContent/WEB-INF/views/common/Top.jsp
+++ WebContent/WEB-INF/views/common/Top.jsp
@@ -50,22 +50,21 @@
 <c:set var="id" value='<%= (String)session.getAttribute("memberId") %>'/>
 <div class="category-container">
     <ul class="category-menu">
-        <li class="category-menu-item">홈</li>
+        <li class="category-menu-item"><a href ="/team3_ShoppingSite/mainPage.do">HOME</a></li>
         
-        <li class="category-menu-item">상의</li>
-        <li class="category-menu-item">하의</li>
-        <li class="category-menu-item">아우터</li>
-        <li class="category-menu-item">ACC</li>
-        <li class="category-menu-item">
-            <a href="member/cart.do">장바구니</a></li>
-        <li class="category-menu-item">마이페이지</li>
+        <li class="category-menu-item"><a href ="/team3_ShoppingSite/category.do?categoryNum=61">TOP</a></li>
+        <li class="category-menu-item"><a href ="/team3_ShoppingSite/category.do?categoryNum=62">PANTS</a></li>
+        <li class="category-menu-item"><a href ="/team3_ShoppingSite/category.do?categoryNum=63">OUTER</a></li>
+        <li class="category-menu-item"><a href ="/team3_ShoppingSite/category.do?categoryNum=64">ETC</a></li>
+        <li class="category-menu-item"><a href="/team3_ShoppingSite/member/cart.do">CART</a></li>
+        <li class="category-menu-item"><a href ="/team3_ShoppingSite/member/myPage.do">MYPAGE</a></li>
         <li class="category-menu-item">
         <c:choose>
        		<c:when test="${id == null}">
-            	<a href="signInPage.do">로그인</a></li>
+            	<a href="/team3_ShoppingSite/signInPage.do">SIGN IN</a></li>
             </c:when>
             <c:otherwise>
-            	<a href="signOut.do">로그아웃</a></li>
+            	<a href="/team3_ShoppingSite/signOut.do">SIGN OUT</a></li>
             </c:otherwise>
         </c:choose>
     </ul>
--- src/kr/or/bit/service/CategoryPageService.java
+++ src/kr/or/bit/service/CategoryPageService.java
@@ -1,5 +1,7 @@
 package kr.or.bit.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,8 +19,55 @@ public class CategoryPageService implements Action {
 		ActionForward forward = new ActionForward();
 		
 		int categoryNum = Integer.parseInt(request.getParameter("categoryNum"));
+		System.out.println("categoryNum :" + categoryNum);
 //		int categoryNum = 15;
-		List<DTOSalePost> salePostList = DAOSalePost.getSalePostListByCategory(categoryNum);
+		//List<DTOSalePost> salePostList = DAOSalePost.getSalePostListByCategory(categoryNum);
+		
+//		private int saleNum;
+//		private int selNum;
+//		private int categoryNum;
+//		private String saleTitle;
+//		private String saleContent;
+//		private Date saleCreatedAt;
+//		private List<String> imageAddrs;
+//		private List<Integer> pNums;
+		
+		List<String> imageAddrs = new ArrayList<String>();
+		imageAddrs.add("Test Addrs1");
+		imageAddrs.add("Test Addrs2");
+		imageAddrs.add("Test Addrs3");
+		
+		List<Integer> pNums = new ArrayList<Integer>();
+		pNums.add(1);
+		pNums.add(2);
+		pNums.add(3);
+		
+		List<DTOSalePost> salePostList = new ArrayList<DTOSalePost>();
+		DTOSalePost samplePost1 = new DTOSalePost(91010, 100, categoryNum, "너무 이쁜 셔츠", "너무 이쁜 셔츠 내용", new Date(), imageAddrs);
+		samplePost1.setpNums(pNums);
+		DTOSalePost samplePost2 = new DTOSalePost(91010, 100, categoryNum, "너무 이쁜 셔츠", "너무 이쁜 셔츠 내용", new Date(), imageAddrs);
+		samplePost2.setpNums(pNums);
+		DTOSalePost samplePost3 = new DTOSalePost(91010, 100, categoryNum, "너무 이쁜 셔츠", "너무 이쁜 셔츠 내용", new Date(), imageAddrs);
+		samplePost3.setpNums(pNums);
+		
+		salePostList.add(samplePost1);
+		salePostList.add(samplePost2);
+		salePostList.add(samplePost3);
+		salePostList.add(samplePost1);
+		salePostList.add(samplePost2);
+		salePostList.add(samplePost3);
+		salePostList.add(samplePost1);
+		salePostList.add(samplePost2);
+		salePostList.add(samplePost3);
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		request.setAttribute("salePostList", salePostList);
 		
 		forward.setRedirect(false);
